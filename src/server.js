6b36af7361ab498a56bc603e28d82143b0a8b625
Genki Hagata <ghagata@hugeinc.com>
2017-01-13 14:14:22 -0800

    Adds static data Adds test routes

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
-const http = require('http'),
-    twilio = require('twilio'),
-    express = require('express'),
-    urlencoded = require('body-parser').urlencoded;
-
-const app = express();
-app.use(urlencoded({ extended: false}));
-
-
 // const accountSid = secrets.secrets.TWACCOUNTSID;
 // const authToken = secrets.secrets.TWAUTHTOKEN;
 const accountSid = process.env.TWACCOUNTSID;
 const authToken = process.env.TWAUTHTOKEN;
 
+// const http = require('http'); //Never used
+const twilio = require('twilio');
+const express = require('express');
+const urlencoded = require('body-parser').urlencoded;
 
-// console.log(secrets);
-// console.log('accountSID: ', accountSid);
-// console.log('authToken: ', authToken);
+// Static data
+import Data from './data';
 
-console.log(`${accountSid} : ${authToken}`);
+const app = express();
+app.use(urlencoded({
+  extended: false
+}));
 
 // Create twilio client with credentials
 const client = require('twilio')(accountSid, authToken);
@@ -26,15 +23,17 @@ const client = require('twilio')(accountSid, authToken);
 app.post('/answer', function(request, response) {
   // Use the Twilio Node.js SDK to build an XML response
   let twiml = new twilio.TwimlResponse();
+  // let fromNumber = request.body.From;
+  twiml.say('Hello, you have reached the network activation center.', {
+    voice: 'alice'
+  });
 
-  twiml.say('Hello, you have reached the network activation center.', { voice: 'alice' });
 
   twiml.redirect('/getId');
 
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
-
 });
 
 app.post('/getId', (request, response) => {
@@ -135,35 +134,64 @@ app.post('/recordMessage', (request, response) => {
 
 app.post('/deployMessages', (request, response) => {
   let twiml = new twilio.TwimlResponse();
-  sendText();
-  twiml.say('Your messages have been sent.');
 
+  for (let contact of Data) {
+    testSendText(contact.phoneNumber, contact.message);
+  }
+
+  twiml.say('Your messages have been sent.');
   response.type('text/xml');
   response.send(twiml.toString());
 });
 
 
-//Helpers
-var sendText = function() {
-  // var client = require('twilio')(accountSid, authToken); 
-  client.messages.create({ 
-      to: "+15104499800", 
-      from: "+16506655133", 
-      body: "Natalia's in trouble. Pick up her kids!", 
-  }, function(err, message) { 
-      console.log(message.sid); 
+// Helpers
+function sendText(number, message) {
+  // var client = require('twilio')(accountSid, authToken);
+
+  client.messages.create({
+    to: number,
+    from: "+15103700864",
+    body: message
+  }, function(err, message) {
+    if (err) {
+      console.log("ERROR:", err);
+    }
+    console.log(message.sid);
   });
-};
+}
 
+// TESTING functions
+// test functions execute the same logic as the "real" functions without
+//  actually sending messages through Twilio.
+//
 
+/**
+ * Test Route
+ */
 app.post('/test', (request, response) => {
-  console.log('\n\ntesting…', request.query)
+  // getUserData();
+  for (let c of Data) {
+    testSendText(c.phoneNumber, c.message);
+  }
+  console.log('your messages have been sent.');
   response.status(200).send('POST request to homepage');
-})
+});
 
+/**
+ * Mimics text messages being sent in the console.
+ * @param {String} number Phone number to send message to
+ * @param {String} message The text message to be sent
+ */
+function testSendText(number, message) {
+  let clientMessage = {
+    to: number,
+    from: "+15103700864",
+    body: message
+  };
+  console.log(clientMessage);
+}
 
-// Create an HTTP server and listen for requests on port 1337
+// Create an HTTP server and listen for requests on port 8080
 app.listen(8080);
-
-console.log('Server running at http://127.0.0.1:1337/');
-
+console.log('Server running at http://127.0.0.1:8080/');

3d5e577456f4f7dfbfb9eaaf8ee852d92e4e3216
Natalia Margolis <=>
2017-01-13 13:06:51 -0800

    Add validateID and showMainOptions

--- src/server.js
+++ src/server.js
@@ -8,27 +8,78 @@ import secrets from './setup.js';
 const app = express();
 app.use(urlencoded({ extended: false}));
 
+// Get account credentials
 const accountSid = secrets.secrets.TWACCOUNTSID;
 const authToken = secrets.secrets.TWAUTHTOKEN;
-console.log(secrets);
-console.log('accountSID: ', accountSid);
-console.log('authToken: ', authToken);
 
+// Create twilio client with credentials
 const client = require('twilio')(accountSid, authToken);
 
+// Route to answer phone
 app.post('/answer', function(request, response) {
   // Use the Twilio Node.js SDK to build an XML response
   let twiml = new twilio.TwimlResponse();
-  // let fromNumber = request.body.From;
+
   twiml.say('Hello, you have reached the network activation center.', { voice: 'alice' });
 
-  twiml.redirect('/deployMessages');
+  twiml.redirect('/getId');
+
 
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
 
 });
+
+app.post('/getId', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
+
+  twiml.gather({ timeout: 10, action: '/validateId'}, (gatherNode) => {
+    gatherNode.say('Please enter your phone number and press pound.', { voice: 'alice'});
+  });
+
+  twiml.redirect('/getId');
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+})
+
+app.post('/validateId', (request, response) => {
+  let twiml = new twilio.TwimlResponse();
+
+  if (request.body.Digits) {
+    let phoneNumber = request.body.Digits;
+    twiml.say('You entered ' + phoneNumber + '.', { voice: 'alice'});
+
+    let userId = phoneNumber;
+
+    let uri = '/showMainOptions/?id=' + userId;
+
+    twiml.redirect(uri);
+  }
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+})
+
+app.post('/showMainOptions', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
+
+  let userId = request.query.id;
+
+  twiml.say('Your id is ' + userId + '.');
+
+  twiml.say('Here are your options.', { voice: 'alice' });
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+})
+
 
 app.post('/deployMessages', (request, response) => {
   let twiml = new twilio.TwimlResponse();
